c7d835d01e8df2cf4a86803cada608af171e3419
jojosimon <37109798+jojosimon@users.noreply.github.com>
2018-03-07 14:56:03 +0100

    Update testeur.c
    salut j'y ai rajouté la fonction numero 7 util collision. Elle marche je l'ai testé

--- Projet/testeur.c
+++ Projet/testeur.c
@@ -25,6 +25,7 @@ double 	util_angle(S2D a, S2D b);
 _Bool   util_point_dehors(S2D a, double max);
 _Bool 	util_alpha_dehors(double alpha);
 _Bool util_point_dans_cercle(S2D b, C2D c);
+_Bool 	util_collision_cercle(C2D a, C2D b, double * p_dist);
 
 int main(void)
 {
@@ -33,6 +34,10 @@ int main(void)
 	S2D a = {3,6};
 	S2D b = {3,5};
 	C2D c ={a,2};
+	C2D d = {b,2};
+	double distance;
+	double *p_dist;
+	p_dist = &distance;
 	angle = util_angle(a,b);
 	printf("\nl'angle est %f",angle);
 	if(util_point_dehors(a, 5)){
@@ -43,6 +48,9 @@ int main(void)
 		}
 	if(util_point_dans_cercle(b,c)){
 		printf("\npoint dans cercle");
+		}
+	if(util_collision_cercle(c, d, p_dist)){
+		printf("le cercle sont en collision en la distance entre les 2 centre est %f \n",distance);
 		}
 
 }
@@ -109,4 +117,12 @@ _Bool 	util_point_dans_cercle(S2D a, C2D c)
 	}
 	return FAUX;
 }
-
+_Bool 	util_collision_cercle(C2D a, C2D b, double * p_dist)
+{
+	*p_dist = util_distance(a.centre,b.centre);
+	if(*p_dist < ((a.rayon + b.rayon)- EPSIL_ZERO))
+	{
+		return VRAIE;
+	}
+	return FAUX;
+}
