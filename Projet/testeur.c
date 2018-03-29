75fb6903867ef8854dfedc3a2b3a01d0ff5f8633
jojosimon <37109798+jojosimon@users.noreply.github.com>
2018-03-29 18:25:04 +0200

    Add files via upload
    Problème avec la conidtion du while tan qu'on lise pas Fin de ligne.. exemple test E01

--- Projet/testeur.c
+++ Projet/testeur.c
@@ -27,29 +27,56 @@ int main (int argc, char* argv[])
 	if(argc !=2)
 	{
 		printf("erreur de lecture \n");
-		ROBOT *tete_liste=NULL;
-		lecture_robots(&tete_liste, argv[1]);
-		return 0;
+		exit(0);
 	}
+
+	ROBOT *tete_liste=NULL;
+	lecture_robots(&tete_liste, argv[1]);
+	liste_afficher(tete_liste);
+	return 0;
 	
 }
 void lecture_robots(ROBOT** tete_liste, char* nom_fichier)
 {
 	int nbbot_att,nbbot_recu, etat=NB_R;
 	int lg_fin = 9;
+	int compteur=0;
 	float a,b,c;
+	int i;
 	ROBOT* courant=NULL;
 	char tab [80];
+	char* deb = tab;
+	char*fin = NULL;
 	FILE * fichier =fopen(nom_fichier,"r");
 	if(fichier)
 	{
-		while(fgets(tab,80,fichier))
+		while((strncmp("FIN_LISTE",tab,lg_fin))!=0)
 		{
-			if((tab[0]=='#')||(tab[0]=='\n')||(tab[0]=='\r'))
-			continue; 
+			i=0;
+			deb = tab;
+			fgets(tab,80,fichier);
+			compteur++;
+			printf("ligne %d \n", compteur);
+			if(compteur ==11)
+			{
+				printf("la valeur de la chaine ligne lu %s",deb);
+				printf("la valeur de strncmp %d",strncmp("FIN_LISTE",tab,lg_fin)); //problème il ne prend pas en compte la condition comme il faut, exemple test E01:
+																						// il lit la ligne 11 alors qu c'est FIN_LISTE
+			}
+
+			while((tab[i]==' ' )||tab[i]=='\t')
+			{
+				i++;
+			}
+			if((tab[i]=='#')||(tab[i]=='\n')||(tab[i]=='\r')||(tab[i]==' ')||(tab[i]=='\t'))
+			{
+				continue;
+			}
+
 			switch (etat)
 			{
 				case NB_R : 
+				printf("compteur switch %d\n",compteur );
 					if (sscanf(tab,"%d",&nbbot_att)==1)
 					{
 						etat++;
@@ -59,24 +86,30 @@ void lecture_robots(ROBOT** tete_liste, char* nom_fichier)
 						printf("erreur il n'y a pas robot"); // fonction erreur à rajouter dans le module error !
 						exit(0); 
 					}
+
+				printf("etat est  %d\n",etat);
 					break;
+
+
 				case RO :
-					if(strncmp("FIN_LISTE",tab,lg_fin)!=0)
+					printf("je lis les donnees \n" );
+					while (sscanf(deb,"%f %f %f", &a,&b,&c) ==3)	//lire les données et les ranger dans le tas dans une liste chaînée
 					{
-						while (sscanf(tab,"%f %f %f", &a,&b,&c)==3) 	//lire les données et les ranger dans le tas dans une liste chaînée
-						{
-							analyse_angle_bot (c);
-							courant = liste_ajouter(tete_liste);
-							courant->numero = nbbot_recu++;
-							courant->corps.x = a;
-							courant->corps.y = b;
-							courant->angle = c;
-						}
+						printf("a%f b%f c%f\n",a,b,c);
+						analyse_angle_bot (c);
+						courant = liste_ajouter(tete_liste);
+						courant->numero = nbbot_recu++;
+						courant->corps.x = a;
+						courant->corps.y = b;
+						courant->angle = c;
+						strtod(deb, &fin); // fonction du cours fichiers
+						deb = (fin+5); //5 parce qu'on compte aussi les espace
 					}
-					break;
+				break;
 			}
-			analyse_nbrbot(nbbot_att, nbbot_recu, __LINE__ );	
-		}
+		}	
+		analyse_nbrbot(nbbot_att, nbbot_recu, compteur );	
+		
 	}
 	fclose(fichier);
 }
@@ -132,6 +165,10 @@ void liste_afficher ( ROBOT *tete )
 			voiture = voiture->suivant;
 		}
 		while((voiture)!=NULL);
+	}
+	else 
+	{
+		printf("erreur de tete liste, ja'rrive pas à lire %s", __func__);
 	}
 
 }
