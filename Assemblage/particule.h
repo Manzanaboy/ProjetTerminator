5862ae8493ea0dba8cf5eeab6a20c9eb77764cee
chevre sama <damireptil@gmail.com>
2018-04-18 16:28:14 +0200

    Update particule.h

--- Assemblage/particule.h
+++ Assemblage/particule.h
@@ -2,17 +2,17 @@
 #define PARTICULES_H\
 
 typedef struct particule PARTICULE;
-void lecture_particules(PARTICULE** tete_liste, char* nom_fichier);
-void analyse_validite_part(double energie, double rayon,double pos_x, 
+void particule_lecture_particules(PARTICULE** tete_liste, char* nom_fichier);
+void particule_analyse_validite_part(double energie, double rayon,double pos_x, 
 														double pos_y);
-int chercheur_ligne(char* nom_fichier);
-PARTICULE * liste_add ( PARTICULE ** p_tete );
-void analyse_nbrpart(int nbpart_att,int nbpart_recu,unsigned int ligne);
-void liste_show ( PARTICULE *tete );
-void part_destruction ( PARTICULE ** p_tete, PARTICULE *el);
-void part_total_destruction(PARTICULE**tete_liste);
-void test3 (void); 
-void passage_donnees(int nbpart_recu, double en, double ray,
+int particule_chercheur_ligne(char* nom_fichier);
+PARTICULE * particule_liste_add ( PARTICULE ** p_tete );
+void particule_analyse_nbrpart(int nbpart_att,int nbpart_recu,unsigned int ligne);
+void particule_liste_show ( PARTICULE *tete );
+void particule_part_destruction ( PARTICULE ** p_tete, PARTICULE *el);
+void particule_part_total_destruction(PARTICULE**tete_liste);
+void particule_test3 (void); 
+void particule_passage_donnees(int nbpart_recu, double en, double ray,
 					double pos_x, double pos_y, PARTICULE*courant);
 
 
