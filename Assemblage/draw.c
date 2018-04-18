28200d964d403203495bb8e49b74503c56f3bd55
jojosimon <37109798+jojosimon@users.noreply.github.com>
2018-04-18 17:01:46 +0200

    Add files via upload

--- Assemblage/draw.c
+++ Assemblage/draw.c
@@ -29,25 +29,31 @@ void draw_robot(double xc, double yc, double angle)
 	glVertex2d(xc+R_ROBOT*cos(angle),yc+R_ROBOT*sin(angle));
 	glEnd();
 
-	glBegin(GL_POINTS);
-	glVertex2f(xc,yc);
-	glColor3f(1,0,0);
+	double petit_rayon = 0.1;
+	glBegin(GL_TRIANGLE_FAN);
+	for (int i=0; i < SIDES; i++)
+    {
+      float alpha = i * 2. * 3.14159265 / SIDES;
+      float px = petit_rayon*cos(alpha);
+      float py = petit_rayon*sin(alpha);
+      glColor3f(1,0,0);
+      glVertex2f(xc+px,yc+py);
+    }
 	glEnd();
 
 }
 
-void draw_part(double xc, double yc)
+void draw_part(double xc, double yc,double rayon)
 {
 	const int SIDES = 50;
-	int r=3;
 
 	glBegin(GL_TRIANGLE_FAN);
 
 	for (int i=0; i < SIDES; i++)
     {
       float alpha = i * 2. * 3.14159265 / SIDES;
-      float x = r*cos(alpha);
-      float y = r*sin(alpha);
+      float x = rayon*cos(alpha);
+      float y = rayon*sin(alpha);
 
       glColor3f(0.5,0.5,0.5);
       glVertex2f(x+xc,y+yc);
--- Assemblage/draw.h
+++ Assemblage/draw.h
@@ -1,7 +1,7 @@
 #ifndef DRAW_H
 #define DRAW_H
 
-void draw_part(double xc, double yc);
+void draw_part(double xc, double yc,double rayon);
 void draw_robot(double xc, double yc, double angle);
 
 #endif
