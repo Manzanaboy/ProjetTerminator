46c2183c591283d386e1682cd49f227f0034e9b6
chevre sama <damireptil@gmail.com>
2018-05-15 11:49:19 +0200

    Update draw.c

--- rendu3_V1.04/draw.c
+++ rendu3_V1.04/draw.c
@@ -49,6 +49,45 @@ void draw_robot(double xc, double yc, double angle)
 
 }
 
+void draw_red(double xc, double yc, double angle)
+{
+  int i=0;
+  float x=0,y=0,px=0,py=0, alpha=0;
+  double petit_rayon = PETIT_R;
+	
+	glBegin(GL_LINE_LOOP);
+
+	for (i=0; i < SIDES; i++)
+    {
+      alpha = i * 2. * M_PI / SIDES;
+      x = R_ROBOT*cos(alpha);
+      y = R_ROBOT*sin(alpha);
+
+      glColor3f(1,0,0);
+      glVertex2f(x+xc,y+yc);
+      
+    }
+	glEnd();
+
+	glBegin(GL_LINE_LOOP);
+	glVertex2d(xc,yc);
+	glVertex2d(xc+R_ROBOT*cos(angle),yc+R_ROBOT*sin(angle));
+	glEnd();
+
+
+	glBegin(GL_TRIANGLE_FAN);
+	for (int i=0; i < SIDES; i++)
+    {
+      alpha = i * 2. * M_PI / SIDES;
+      px = petit_rayon*cos(alpha);
+      py = petit_rayon*sin(alpha);
+      glColor3f(1,0,0);
+      glVertex2f(xc+px,yc+py);
+    }
+	glEnd();
+
+}
+
 void draw_part(double xc, double yc,double rayon)
 {
   int i=0;
