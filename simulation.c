0976c05d260e3ad6336a86c5ab941975b429a322
jojosimon <37109798+jojosimon@users.noreply.github.com>
2018-05-21 18:21:45 +0200

    Add files via upload

---
+++ simulation.c
@@ -0,0 +1,100 @@
+/*!
+ \file simulation.c
+ \brief Module qui gère le traitement d'un fichier en fonction du mode
+		que l'on veut.
+ \author Jose Antonio Simon Greminger
+		 Damian Perez Grether
+		 Selina Bothner
+ \version 3.01
+ \date 21 avril 2018
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include "utilitaire.h"
+#include "error.h"
+#include "constantes.h"
+#include "simulation.h"
+#include "robot.h"
+#include "particule.h"
+#include "draw.h"
+
+enum Power {DESTROY,CREATE};
+
+void simulation_first_lecture(char* nom_fichier,
+									int*p_base,char* mode_lecture)	
+{
+	int ok=CREATE;
+	int *p_ok=&ok;
+	lecture_robots(nom_fichier,mode_lecture,p_ok);
+	lecture_particules(nom_fichier,mode_lecture,p_ok);
+	if(ok)
+	{
+		robot_collision_bot_bot(mode_lecture,p_ok);
+		particule_collision_part_part(mode_lecture,p_ok);
+		robot_collisions_bot_part (mode_lecture,p_ok);
+		
+	}
+	if(!(strncmp(mode_lecture,"Draw",4)))
+	{
+		simulation_dessiner();
+		if(!ok)
+		{
+			*p_base=DESTROY;
+		}
+	}
+	else if(!(strncmp(mode_lecture,"Error",5)))
+	{
+		error_no_error_in_this_file();
+	}
+	robot_assoc_robot_part();
+	
+}
+
+void simulation_mja()
+{
+	robot_assoc_robot_part();
+	simulation_dessiner();
+}
+
+void simulation_detruire()
+{
+	//~ simulation_mja();
+	bot_total_destruction();
+	part_total_destruction();
+}
+
+void simulation_dessiner()
+{
+	draw_cadre();
+	robot_dessin();
+	particule_dessin();
+}
+
+void simulation_decomposition()
+{
+	int decomposition =0;
+	decomposition = part_decomposition_start();
+	//~ printf("decompositioon est %d f.%s\n",decomposition,__func__);
+	if(decomposition)
+	{
+		simulation_mja();
+	}
+}
+
+int simulation_particules()
+{
+	PARTICULE*courant=NULL;
+	courant=particule_acces_tete();
+	if(courant)
+	{
+		return 1;
+	}
+	else 
+	{
+		return 0;
+	}
+}
---
+++ simulation.h
@@ -0,0 +1,55 @@
+/*!
+ \file simulation.h
+ \brief Module qui gère le traitement d'un fichier en fonction du mode
+		que l'on veut.
+ \author Jose Antonio Simon Greminger
+		 Damian Perez Grether
+		 Selina Bothner
+ \version 3.01
+ \date 21 avril 2018
+ */
+
+#include "robot.h"
+#include "particule.h"
+
+
+/**
+ * \brief  lire le fichier, et dire s'il faut dessiner une
+ * 			page blanche ou une page de dessin
+ * \param 	tete_liste_bot		pointeur sur la tete de la liste 
+ * 								des robots.
+ * \param   mode_lecture		Error ou Draw
+ * \param   nom_fichier			nom du fichier qu'on veut lire
+ * \param 	tete_liste_part		pointeur sur la tete de la liste 
+ * 								des particules.
+ * \param 	base				pointeur qui va indiquer si la base sera
+ * 								une page blanche une page de dessin
+ */
+void simulation_first_lecture(char* nom_fichier,
+									int*p_base,char* mode_lecture);
+
+/**
+ * \brief  Détruire les données et mettre les pointeurs à NULL
+ * \param 	tete_liste_bot		pointeur sur la tete de la liste 
+ * 								des robots.
+ * \param 	tete_liste_part		pointeur sur la tete de la liste 
+ * 								des particules.
+ */
+void simulation_detruire();
+
+/**
+ * \brief  Se charger du developpement de la simulation
+ */
+void simulation_mja();
+
+/**
+ * \brief  Se charger du dessin de la simulation
+ */
+void simulation_dessiner();
+
+/**
+ * \brief  Se charger de la decompostion des particules
+ */
+void simulation_decomposition();
+
+int simulation_particules();
